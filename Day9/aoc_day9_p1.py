60e63b8709b12bce580a3702519061b5f5facd00
mauricekoller <57102706+mauricekoller@users.noreply.github.com>
2022-12-10 16:29:03 +0100

    Day9 P1

--- Day9/aoc_day9_p1.py
+++ Day9/aoc_day9_p1.py
@@ -1,9 +1,10 @@
 with open('Day9/input.txt', 'r') as input:
     text_lines = input.read().rstrip().splitlines()
-    position_head = [0,0]
-    position_tail = [0,0]
+    position_head = [0, 0]
+    position_tail = [0, 0]
 
-    tail_positions = []
+    # Add starting position by default
+    tail_positions = set([(0, 0)])
 
     # "Array" starts at [0,0] increasing to the right/upwards
     for line in text_lines:
@@ -11,21 +12,33 @@ with open('Day9/input.txt', 'r') as input:
 
         # Move Head one position at a time
         # Afterwards check if Distance to Tail exists and move it accordingly if necessary
-        # After move add new position of Tail to List? --> At the End make a set from the list to remove duplicates! and count
-        for _ in range(move_len):
+        # After move add new position of Tail as a Tuple to Set --> No duplicates
+        for _ in range(int(move_len)):
+
             # Move Head
             match move_dir:
                 case 'L':
-                    position_head[0]-=1
+                    position_head[0] -= 1
                 case 'R':
-                    position_head[0]+=1
+                    position_head[0] += 1
                 case 'U':
-                    position_head[1]+=1
+                    position_head[1] += 1
                 case 'D':
-                    position_head[1]-=1
-                
-            # Check if tail needs to be moved
-            
+                    position_head[1] -= 1
 
+            # Check if Tail and Head are seperated
+            # If Distance is bigger than one: Move Tail one position towards Head, If Distance is Diagonal, move Diagonally towards Head
+            # .....    .....    .....
+            # .....    ..H..    ..H..
+            # ..H.. -> ..... -> ..T..
+            # .T...    .T...    .....
+            # .....    .....    .....
+            distance = [h-t for h, t in zip(position_head, position_tail)]
+            # Create movement instruction: If Distance is 2 subtract/add one, else keep it the same
+            # Examples: [2,1] -move-> [1,1]     [-2,0] -move-> [-1,0]       [2,0] -move-> [1,0]
+            if any(abs(value) >= 2 for value in distance):
+                tail_movement = [val-1 if val == 2 else val+1 if val == -2 else val for val in distance]
+                position_tail = [t+m for t, m in zip(position_tail, tail_movement)]
+                tail_positions.add(tuple(position_tail))
 
-    print(f'Number of all visible Trees: {0}')
+    print(f'Number of Positions the Tail visited: {len(tail_positions)}')
